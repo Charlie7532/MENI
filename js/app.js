afb17a3667f64fd260bf57f959578cb26b15fd25
CHARLIE <jcarlosbotero1@outlook.com>
2023-02-07 08:49:24 -0500

    materialize carrusel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+/** @format */
 
+document.addEventListener('DOMContentLoaded', function () {
+	var elems = document.querySelectorAll('.sidenav');
+	M.Sidenav.init(elems, { edge: 'right' });
+
+	// setTimeout(showSlides, 3000);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth',
+		});
+	});
+});
