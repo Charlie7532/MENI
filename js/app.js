39183246e70866cb7abf693249a3f3e9d74c96e4
CHARLIE <jcarlosbotero1@outlook.com>
2023-02-12 23:17:06 -0500

    Update Slider Working

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
 /** @format */
 document.addEventListener('DOMContentLoaded', function () {
-	M.Sidenav.init(document.querySelectorAll('.sidenav'), { edge: 'right' });
-	var Baner = M.Carousel.init(document.querySelectorAll('.carousel'), {
+	let instance_Sidenav = M.Sidenav.init(document.querySelectorAll('.sidenav'), { edge: 'right' });
+	let instance = M.Carousel.init(document.querySelectorAll('.carousel'), {
 		fullWidth: true,
 		indicators: true,
-		duration: 200,
+		duration: 500,
+		interval: 5000,
+	});
+
+
+    var instances = M.Slider.init(document.querySelectorAll('.slider'), {
+		fullWidth: true,
+		indicators: true,
+		duration: 3000,
+		interval: 6000
 	});
 
 	// setTimeout(() => {
-	// 	Baner.next();
+	// 	var elem = document.querySelectorAll('.carousel');
+	// 	var instance = M.Carousel.getInstance(elem);
+	// 	instance.next();
 	// }, 3000);
+
+	// setInterval(()=>{
+
+	// });
 });
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-	anchor.addEventListener('click', function (e) {
-		e.preventDefault();
-		document.querySelector(this.getAttribute('href')).scrollIntoView({
-			behavior: 'smooth',
-		});
-	});
-});
-
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+// 	anchor.addEventListener('click', function (e) {
+// 		e.preventDefault();
+// 		document.querySelector(this.getAttribute('href')).scrollIntoView({
+// 			behavior: 'smooth',
+// 		});
+// 	});
+// });
 
 //create a contact\
-
-
