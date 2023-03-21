b93ab827a233cc604e2635cb233562ea9bf8b9e0
CHARLIE <jcarlosbotero1@outlook.com>
2023-03-21 17:20:39 -0400

    update

--- js/app.js
+++ js/app.js
@@ -2,33 +2,21 @@
 let test = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-	M.Sidenav.init(document.querySelectorAll('.sidenav'), { edge: 'right' });
-	// let instance = M.Carousel.init(document.querySelectorAll('.carousel'), {
-	// 	fullWidth: true,
-	// 	indicators: true,
-	// 	duration: 500,
-	// 	interval: 5000,
-	// });
-
-    M.Parallax.init(document.querySelectorAll('.parallax'));
-
-	M.Modal.init(document.querySelectorAll('.modal'), {
-		opacity: 0.5,
-		preventScrolling: true,
-		dismissible: true,
-	});
-
 	M.Slider.init(document.querySelectorAll('.slider'), {
 		fullWidth: true,
 		indicators: true,
 		duration: 2500,
 		interval: 5000,
 	});
+	M.Parallax.init(document.querySelectorAll('.parallax'));
+	M.Sidenav.init(document.querySelectorAll('.sidenav'), { edge: 'right' });
+	M.Modal.init(document.querySelectorAll('.modal'), {
+		opacity: 0.5,
+		preventScrolling: true,
+		dismissible: true,
+	});
 });
 
-// $(document).ready(function () {
-// 	$('input#input_text, textarea#textarea2').characterCounter();
-// });
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 	anchor.addEventListener('click', function (e) {
